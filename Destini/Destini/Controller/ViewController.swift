22fa585991126205d9dd1f77b77b463ba3fc3cb1
Berkay Kuzu <92036779+Berkay-Kuzu@users.noreply.github.com>
2022-09-05 13:51:20 +0300

    Add files via upload

---
+++ Destini/Destini/Controller/ViewController.swift
@@ -0,0 +1,66 @@
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
+    var storyBrain = StoryBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateUI()
+
+    }
+
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+        storyBrain.nextStory(userChoice: sender.currentTitle!)
+        
+        updateUI()
+     
+    }
+    
+    func updateUI() {
+        storyLabel.text = storyBrain.getStoryTitle()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
